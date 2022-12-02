942eb1fdd04cd04bd93de69352f25f641f273308
Joe Rutkin <rutkinj@gmail.com>
2022-12-01 17:07:57 -0800

    pages are linked together

--- app/src/main/java/com/jrutkin/listwiz/MainActivity.java
+++ app/src/main/java/com/jrutkin/listwiz/MainActivity.java
@@ -2,7 +2,9 @@ package com.jrutkin.listwiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addTaskButton = MainActivity.this.findViewById(R.id.MainButtonAddTask);
+        addTaskButton.setOnClickListener(view -> {
+            Intent goToAddTaskActivity = new Intent(this, AddTask.class);
+            startActivity(goToAddTaskActivity);
+        });
+
+        Button allTasksButton = MainActivity.this.findViewById(R.id.MainButtonAllTasks);
+        allTasksButton.setOnClickListener(view -> {
+            Intent goToAllTasksActivity = new Intent(this, AllTasks.class);
+            startActivity(goToAllTasksActivity);
+        });
     }
 }
