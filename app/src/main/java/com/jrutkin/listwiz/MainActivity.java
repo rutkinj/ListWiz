6fa8dbed4ff4c1586a7d8a1779b42ef3631ac2a0
Joe Rutkin <rutkinj@gmail.com>
2022-12-04 15:50:07 -0800

    add baseline of task detail activity

--- app/src/main/java/com/jrutkin/listwiz/MainActivity.java
+++ app/src/main/java/com/jrutkin/listwiz/MainActivity.java
@@ -3,8 +3,12 @@ package com.jrutkin.listwiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonSetup();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        greetingSetup();
     }
 
     private void buttonSetup(){
@@ -28,5 +39,18 @@ public class MainActivity extends AppCompatActivity {
             Intent goToAllTasksActivity = new Intent(this, AllTasks.class);
             startActivity(goToAllTasksActivity);
         });
+
+        ImageView toUserProfileIV = MainActivity.this.findViewById(R.id.MainIVToUserProfile);
+        toUserProfileIV.setOnClickListener(view -> {
+            Intent goToUserProfileActivity = new Intent(this, UserProfileActivity.class);
+            startActivity(goToUserProfileActivity);
+        });
+    }
+
+    public void greetingSetup(){
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String username = sharedPrefs.getString(UserProfileActivity.USERNAME_TAG, "No username");
+
+        ((TextView)findViewById(R.id.MainTVUsername)).setText(username);
     }
 }
