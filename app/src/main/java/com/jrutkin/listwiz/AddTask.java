7b866848da06149abae9d2f45bdcb3f7c6decef4
Joe Rutkin <rutkinj@gmail.com>
2022-12-04 13:12:54 -0800

    small refactor to cleanup onCreate

--- app/src/main/java/com/jrutkin/listwiz/AddTask.java
+++ app/src/main/java/com/jrutkin/listwiz/AddTask.java
@@ -2,9 +2,11 @@ package com.jrutkin.listwiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddTask extends AppCompatActivity {
 
@@ -13,11 +15,16 @@ public class AddTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
-        Button addTaskButton = AddTask.this.findViewById(R.id.AddButton);
-        addTaskButton.setOnClickListener(view ->{
-            TextView title = AddTask.this.findViewById(R.id.AddTVTitle);
-            title.setText("Submitted but I don't how to do a toast");
-        });
+        buttonSetup();
     }
 
+    private void buttonSetup(){
+        Button addTaskButton = AddTask.this.findViewById(R.id.AddButton);
+        addTaskButton.setOnClickListener(view ->{
+            Context context = getApplicationContext();
+            CharSequence submitMsg = "Task Submitted";
+            Toast toast = Toast.makeText(context, submitMsg, Toast.LENGTH_SHORT);
+            toast.show();
+        });
+    }
 }
--- app/src/main/java/com/jrutkin/listwiz/MainActivity.java
+++ app/src/main/java/com/jrutkin/listwiz/MainActivity.java
@@ -13,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonSetup();
+    }
+
+    private void buttonSetup(){
         Button addTaskButton = MainActivity.this.findViewById(R.id.MainButtonAddTask);
         addTaskButton.setOnClickListener(view -> {
             Intent goToAddTaskActivity = new Intent(this, AddTask.class);
