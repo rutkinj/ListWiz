f970d80d043bfbb6cbd4c1e31fea44bd426a7d50
Joe Rutkin <rutkinj@gmail.com>
2022-12-04 15:49:19 -0800

    add user profile activity

---
+++ app/src/main/java/com/jrutkin/listwiz/UserProfileActivity.java
@@ -0,0 +1,41 @@
+package com.jrutkin.listwiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPrefs;
+    public static final String USERNAME_TAG = "username";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        saveToSharedPrefs();
+    }
+
+    public void saveToSharedPrefs(){
+        SharedPreferences.Editor prefEditor = sharedPrefs.edit();
+        Button submitButton = UserProfileActivity.this.findViewById(R.id.UserProfileSubmitButton);
+
+        submitButton.setOnClickListener(view -> {
+            EditText usernameET = findViewById(R.id.UserProfileETUsername);
+            String usernameText = usernameET.getText().toString();
+            prefEditor.putString(USERNAME_TAG, usernameText);
+
+            prefEditor.apply();
+
+            Toast.makeText(this, "Username Saved", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
